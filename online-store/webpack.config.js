6c8e049c1a1c58ccedffee808e9ed7b8215f6897
Pavel Kizhlo <pavelkizh@gmail.com>
2022-07-08 20:59:51 +0300

    feat: add adaptive to appView, add favicon

--- online-store/webpack.config.js
+++ online-store/webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const baseConfig = {
   entry: path.resolve(__dirname, './src/index.ts'),
@@ -31,6 +32,11 @@ const baseConfig = {
       filename: 'index.html',
     }),
     new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: './src/assets/favicon.ico', to: '../dist' },
+      ],
+    })
   ],
 };
 
