e7aff883a759b397f1edf6bf25c06bfa4f3baf98
Pavel Kizhlo <pavelkizh@gmail.com>
2022-07-13 13:52:38 +0300

    feat: implement filters reset by button click

--- online-store/src/components/catalog-page/filter/filter.ts
+++ online-store/src/components/catalog-page/filter/filter.ts
@@ -1,4 +1,5 @@
 import { View } from '../../../interfaces/interfaces';
+import Search from '../search/search';
 import './filter.sass';
 import noUiSlider, { Formatter, target, API } from 'nouislider';
 import 'nouislider/dist/nouislider.css';
@@ -130,7 +131,7 @@ class Filter implements View {
               <label class="label" for="is-popular">Popular only</label>
             </div>
           </div>
-          <button class="filters__clear">Clear Filters</button>
+          <button class="filters__clear" id="clear-filters">Clear Filters</button>
         `;
 
         const priceSlider = document.getElementById('price-slider') as HTMLDivElement;
@@ -167,6 +168,22 @@ class Filter implements View {
 
         this.mergeTooltips(priceSlider, 30, ' - ');
         this.mergeTooltips(amountSlider, 20, ' - ');
+
+        (document.getElementById('clear-filters') as HTMLButtonElement).addEventListener('click', () => {
+            filters.querySelectorAll('input').forEach((input) => {
+                if (input.type === 'radio') {
+                    input.checked = input.id === 'all-movements' ? true : false;
+                } else {
+                    input.checked = false;
+                }
+
+                ((priceSlider as target).noUiSlider as API).set([0, 300]);
+                ((amountSlider as target).noUiSlider as API).set([0, 100]);
+
+                const search = new Search();
+                search.render();
+            });
+        });
     }
 
     private mergeTooltips(slider: target, threshold: number, separator: string) {
